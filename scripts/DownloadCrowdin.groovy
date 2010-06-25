58fa76a4cba5209e98bf3719d820721d79c971e9
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-06-25 16:42:20 -0300

    Now export before download and overwrites old files

--- scripts/DownloadCrowdin.groovy
+++ scripts/DownloadCrowdin.groovy
@@ -4,13 +4,14 @@ target(main: "Download translations from crowdin and replace them with the curre
     def config = new ConfigSlurper().parse(new File('grails-app/conf/CrowdinConf.groovy').toURL())
     delete dir:"target/translationTemp"
     mkdir dir:"target/translationTemp"
+    get src:"http://crowdin.net/api/project/${config.crowdin.projectid}/export?key=${config.crowdin.apikey}", dest:"export.log"
     get src:"http://crowdin.net/api/project/${config.crowdin.projectid}/download/all.zip?key=${config.crowdin.apikey}", dest:"target/translationTemp/all.zip"
     unzip src:"target/translationTemp/all.zip", dest:"target/translationTemp"
     delete file:"target/translationTemp/all.zip"
     def list = new File("target/translationTemp")
     list.eachFile {
         def lang = it.name.replaceAll(/\-/, "_")
-        copy file:"${it}/Messages.po", tofile:"grails-app/i18n/${lang}.po"
+        copy file:"${it}/Messages.po", tofile:"grails-app/i18n/${lang}.po", overwrite:true, verbose:true
     }    
 }
 
