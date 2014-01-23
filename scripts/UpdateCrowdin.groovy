edcfa3e8b2631516cd6fb039e45030a3911520dd
nsuarezmeli <nicolas.suarez@mercadolibre.com>
2014-01-23 15:37:47 -0200

    https in upload urls

--- scripts/UpdateCrowdin.groovy
+++ scripts/UpdateCrowdin.groovy
@@ -3,7 +3,7 @@ includeTargets << grailsScript("Init")
 target(main: "Update crowdin translation files") {
     def config = new ConfigSlurper().parse(new File('grails-app/conf/CrowdinConf.groovy').toURL())
     exec executable:"curl", {
-        arg line:"-F \"files[Messages.po]=@grails-app/i18n/Messages.po\" http://crowdin.net/api/project/${config.crowdin.projectid}/update-file?key=${config.crowdin.apikey}"
+        arg line:"-F \"files[Messages.po]=@grails-app/i18n/Messages.po\" https://crowdin.net/api/project/${config.crowdin.projectid}/update-file?key=${config.crowdin.apikey}"
     }
 }
 
